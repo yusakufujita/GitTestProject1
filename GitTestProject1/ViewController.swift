1601d3d77632f904d5bdecf63ce668c47b8f0b93
藤田優作 <hinasakun@outlook.jp>
2020-03-12 17:42:20 +0900

    Initial Commit

---
+++ GitTestProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject1
+//
+//  Created by 藤田優作 on 2020/03/12.
+//  Copyright © 2020 藤田優作. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
